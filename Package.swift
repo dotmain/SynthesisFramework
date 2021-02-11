d67c0b0de337d04d9dbad938c1bbc503dceb2a4d
sina.main <>
2021-02-11 23:06:07 +0100

    First Commit: Synthesis Framework - mainvolume

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SynthesisFramework",
-    platforms: [
-        .iOS(.v13), .macOS(.v10_15)
-    ],
+    platforms: [ .iOS(.v13) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
